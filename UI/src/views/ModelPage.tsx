816356d3d3698b904bc4136b7a4793daa455f619
krasilnikov.orchid@gmail.com <krasilnikov.orchid@gmail.com>
2025-02-22 20:00:17 +0100

    feat: add PrintNow button

---
+++ UI/src/views/ModelPage.tsx
@@ -0,0 +1,12 @@
+import PrintNowButton from "@/components/PrintNowButton";
+
+const ModelPage = () => {
+
+    return(
+        <>
+            <PrintNowButton fileName={'valentine_rose.stl'} />
+        </>
+    );
+}
+
+export default ModelPage;
