d0a0ce4d611fbd38ec768f1077eade66074a55b5
krasilnikov.orchid@gmail.com <krasilnikov.orchid@gmail.com>
2025-02-22 19:13:04 +0100

    refactor: move bridge scripts to scripts folder

--- Bridge/scripts/init.js
+++ Bridge/scripts/init.js
@@ -1,8 +1,8 @@
 const { exec } = require('child_process');
 const path = require('path');
 
-const agentPath = path.resolve(__dirname, 'cura-agent.exe');
-
+const agentPath = path.resolve(__dirname, '../output/cura-agent.exe');
+console.log(agentPath);
 const regCommand = `reg add "HKCR\\cura" /ve /d "URL:Cura Protocol" /f && reg add "HKCR\\cura" /v "URL Protocol" /d "" /f && reg add "HKCR\\cura\\shell\\open\\command" /ve /d "\\"${agentPath}\\" \\"%1\\"" /f`;
 
 exec(regCommand, (error, stdout, stderr) => {
