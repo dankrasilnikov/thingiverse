2dcc53375b60bb4106a8f85a681c501ca273b3b8
krasilnikov.orchid@gmail.com <krasilnikov.orchid@gmail.com>
2025-02-26 22:11:32 +0100

    feat: add webpack config

--- ui_next/next.config.ts
+++ ui_next/next.config.ts
@@ -4,10 +4,39 @@ const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'https',
-                hostname: 'cdn.krasilnikov.info',
+                protocol: "https",
+                hostname: "cdn.krasilnikov.info",
             },
         ],
+    },
+
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"],
+        });
+
+        return config;
+    },
+
+    eslint: {
+        ignoreDuringBuilds: false,
+    },
+
+    compress: true,
+
+    headers: async () => {
+        return [
+            {
+                source: "/static/(.*)",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "public, max-age=31536000, immutable",
+                    },
+                ],
+            },
+        ];
     },
 };
 
