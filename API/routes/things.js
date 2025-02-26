af740f696bed7b2161bcb54089e456d011594ac5
krasilnikov.orchid@gmail.com <krasilnikov.orchid@gmail.com>
2025-02-26 15:59:16 +0100

    fix: remove gltf imports from server

--- API/routes/things.js
+++ API/routes/things.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {Client} from '@elastic/elasticsearch';
 import {uploadToR2} from "../services/uploadToR2.js";
-import {convertStlToGltf} from "../services/StlToGltfConvertor.js";
 import {processImage} from "../services/imageConvert.js";
 import multer from "multer";
 
@@ -14,9 +13,8 @@ const upload = multer({
 });
 
 const multipleUpload = upload.fields([
-    { name: 'stls', maxCount: 10 },
-    { name: 'gltfs', maxCount: 10 },
-    { name: 'images', maxCount: 10 },
+    {name: 'stls', maxCount: 10},
+    {name: 'images', maxCount: 10},
 ]);
 
 router.post('/', multipleUpload, async (req, res) => {
@@ -33,7 +31,6 @@ router.post('/', multipleUpload, async (req, res) => {
         }
 
         const stlFiles = req.files['stls'] || [];
-        const gltfFiles = req.files['gltfs'] || []
         const imageFiles = req.files['images'] || [];
 
         const imagesResult = [];
@@ -41,22 +38,16 @@ router.post('/', multipleUpload, async (req, res) => {
 
         for (let i = 0; i < stlFiles.length; i++) {
             const stlFile = stlFiles[i];
-            const gltfFile = gltfFiles[i];
 
             const originalName = stlFile.originalname;
             const stlBuffer = stlFile.buffer;
-            const gltfBuffer = gltfFile.buffer;
 
             const date = Date.now();
 
             const stlKey = `stls/${date}-${originalName}`;
             const stlUrl = await uploadToR2(stlKey, stlBuffer, 'application/sla');
 
-
-            const gltfKey = `gltfs/${date}-${originalName}.gltf`;
-            const gltfUrl = await uploadToR2(gltfKey, gltfBuffer, 'application/json');
-
-            filesResult.push({stlUrl, gltfUrl});
+            filesResult.push(stlUrl);
         }
 
         for (const file of imageFiles) {
