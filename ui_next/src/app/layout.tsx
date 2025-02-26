f9c64f83e68d27e3de73ad4428d7d4f349ca7861
krasilnikov.orchid@gmail.com <krasilnikov.orchid@gmail.com>
2025-02-26 02:28:41 +0100

    feat: add 404 page

--- ui_next/src/app/layout.tsx
+++ ui_next/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.scss";
 import ExternalScripts from "@/config/ExternalScripts";
+import React from "react";
 
 const inter = Inter({
     variable: "--font-inter",
