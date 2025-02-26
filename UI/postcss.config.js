eb1314b784bda5a3ce7d02520caa0be9c32398c1
krasilnikov.orchid@gmail.com <krasilnikov.orchid@gmail.com>
2025-02-26 22:33:41 +0100

    feat: update stylelint plugin & setup postcss config

--- UI/postcss.config.js
+++ UI/postcss.config.js
@@ -6,8 +6,6 @@ module.exports = {
     require('postcss-extend'),
     require('precss'),
     require('postcss-preset-env'),
-    // uncomment if you're using Tailwind
-    // require('tailwindcss')('tailwind.config.js'),
     require('postcss-nested'),
     require('autoprefixer')(),
     require('postcss-reporter'),
---
+++ ui_next/postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    plugins: {
+        stylelint: {
+            configFile: 'stylelint.config.js',
+        },
+        'postcss-extend': {},
+        precss: {},
+        'postcss-preset-env': {},
+        'postcss-nested': {},
+        autoprefixer: {},
+        'postcss-reporter': {},
+    },
+};
