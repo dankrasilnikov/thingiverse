a4e73792b6c9ecfca923f8db9342fbf53acbb498
krasilnikov.orchid@gmail.com <krasilnikov.orchid@gmail.com>
2025-02-26 01:10:25 +0100

    feat: add google tag manager as third party

--- ui_next/src/config/ExternalScripts.tsx
+++ ui_next/src/config/ExternalScripts.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Script from "next/script";
+import {GoogleTagManager} from '@next/third-parties/google'
 
 
 const ExternalScripts: React.FC<{}> = () => {
@@ -7,20 +8,7 @@ const ExternalScripts: React.FC<{}> = () => {
         <>
             <Script strategy={'afterInteractive'} src='https://static.cloudflareinsights.com/beacon.min.js'
                     data-cf-beacon='{"token": "11d17913667f4364a2890c9bbb6b3665"}' defer/>
-            <Script id="gtm-script" strategy="afterInteractive">
-                {`
-                  (function(w,d,s,l,i){
-                    w[l]=w[l]||[];
-                    w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-                    var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),
-                        dl=l!='dataLayer'?'&l='+l:'';
-                    j.async=true;
-                    j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                    f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','GTM-MWWFJBPW');
-                `}
-            </Script>
+            <GoogleTagManager gtmId="GTM-MWWFJBPW"/>
         </>
     );
 }
