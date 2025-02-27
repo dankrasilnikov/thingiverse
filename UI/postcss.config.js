067b0e27edeb143223759377713480e9b08725a4
krasilnikov.orchid@gmail.com <krasilnikov.orchid@gmail.com>
2025-02-27 03:50:05 +0100

    fix: remove precss & add postscss

--- UI/postcss.config.js
+++ UI/postcss.config.js
@@ -1,10 +1,10 @@
 module.exports = {
+  syntax: 'postcss-scss',
   plugins: {
     stylelint: {
       configFile: 'stylelint.config.js',
     },
     'postcss-extend': {},
-    precss: {},
     'postcss-preset-env': {},
     'postcss-nested': {},
     autoprefixer: {},
